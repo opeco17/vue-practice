6741acedee2d64c766cbe37f0bda3928b2cb831f
opeco17 <opeco17@gmail.com>
2021-05-01 08:01:26 +0900

    first commit

---
+++ chapter2/app.js
@@ -0,0 +1,13 @@
+var items = [
+    {name: '鉛筆', price: 300, quantity: 0},
+    {name: 'ノート', price: 400, quantity: 0},
+    {name: '消しゴム', price: 500, quantity: 0}
+]
+var vm = new Vue({
+    el: '#app',
+    data: {
+        items: items
+    }
+})
+
+vm.$watch(() => {return this.items[0].quantity}, (quantity) => {console.log(`数量の変更${quantity}`)});
